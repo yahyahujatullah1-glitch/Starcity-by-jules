826ea2edeca512df54d88b1ebc7c8fe974c0c6dd
yahyahujatullah1-glitch <yahyahujatullah1@gmail.com>
2025-12-10 15:44:54 +0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// YOUR CREDENTIALS
 const SUPABASE_URL = "https://riluxnxxndwocrjuwzpd.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbHV4bnh4bmR3b2NyanV3enBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwOTM1MDEsImV4cCI6MjA4MDY2OTUwMX0.xcLBQijXfbCB3dM1FH4uzo08IPs-trovOy6T_vdpc_o";
 
